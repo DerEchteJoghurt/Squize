876a97b5e1c3da11fa06052349d7a6a248fb0c32
Chaoschaps <74681462+Chaoschaps@users.noreply.github.com>
2024-05-28 14:08:24 +0200

    Update quiz.js

--- services/quiz.js
+++ services/quiz.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const quizDao = require('../dao/quizDao.js')
+const QuizDao = require('../dao/quizDao.js')
 var serviceRouter = express.Router();
 
 serviceRouter.post('/profile/create_quiz', function(request, response) {
-    let quizDao = new quizDao(request.app.locals.dbConnection);
+    let quizDao = new QuizDao(request.app.locals.dbConnection);
     let res = quizDao.createQuiz(request.body);
     response.status(400).json(res)
 });
 
 serviceRouter.post('/profile/update_quiz', function(request, response) {
-    let quizDao = new quizDao(request.app.locals.dbConnection);
+    let quizDao = new QuizDao(request.app.locals.dbConnection);
     let res = quizDao.updateQuiz(request.body);
     response.status(400).json(res)
 });
