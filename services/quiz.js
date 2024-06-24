309391b85cfb5bcf9ff3d9a1be70456e71d2f746
Chaoschaps <chaoschaps.he@protonmail.ch>
2024-06-24 23:18:42 +0200

    add quizzes with answers to database

--- services/quiz.js
+++ services/quiz.js
@@ -5,31 +5,55 @@ var serviceRouter = express.Router();
 serviceRouter.post('/quiz/create_quiz', function(request, response) {
     let quizDao = new QuizDao(request.app.locals.dbConnection);
     let res = quizDao.createQuiz(request.body);
-    response.status(400).json(res)
+    if (res.hasOwnProperty("errormsg")){
+        response.status(400).json(res)
+    }
+    response.status(200).json(res)
 });
 
 serviceRouter.post('/quiz/update_quiz', function(request, response) {
     let quizDao = new QuizDao(request.app.locals.dbConnection);
     let res = quizDao.updateQuiz(request.body);
-    response.status(400).json(res)
+    if (res.hasOwnProperty("errormsg")){
+        response.status(400).json(res)
+    }
+    response.status(200).json(res)
+});
+
+serviceRouter.delete('/quiz/delete_quiz', function(request, response) {
+    let quizDao = new QuizDao(request.app.locals.dbConnection);
+    let res = quizDao.deleteQuiz(request.body);
+    if (res.hasOwnProperty("errormsg")){
+        response.status(400).json(res)
+    }
+    response.status(200).json(res)
 });
 
 serviceRouter.post('/quiz/add_question', function(request, response) {
     let quizDao = new QuizDao(request.app.locals.dbConnection);
     let res = quizDao.addQuestion(request.body);
-    response.status(400).json(res)
+    if (res.hasOwnProperty("errormsg")){
+        response.status(400).json(res)
+    }
+    response.status(200).json(res)
 });
 
 serviceRouter.post('/quiz/update_question', function(request, response) {
     let quizDao = new QuizDao(request.app.locals.dbConnection);
     let res = quizDao.updateQuestion(request.body);
-    response.status(400).json(res)
+    if (res.hasOwnProperty("errormsg")){
+        response.status(400).json(res)
+    }
+    response.status(200).json(res)
 });
 
-serviceRouter.post('/quiz/delete_question', function(request, response) {
+serviceRouter.delete('/quiz/delete_question', function(request, response) {
     let quizDao = new QuizDao(request.app.locals.dbConnection);
     let res = quizDao.deleteQuestionQuestion(request.body);
-    response.status(400).json(res)
+    if (res.hasOwnProperty("errormsg")){
+        response.status(400).json(res)
+    }
+    response.status(200).json(res)
 });
 
 module.exports = serviceRouter;
